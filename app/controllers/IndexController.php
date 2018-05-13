9332b54a243b40019b008c23cdb37743af51c139
Sean Jorgensen <jorgensean@gmail.com>
2018-05-13 15:08:41 -0700

    Add templates for registration and success page

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,12 +5,12 @@ class IndexController extends ControllerBase
 
     public function indexAction()
     {
-
+      $this->assets->addCss("css/registration.css");
     }
 
     public function successAction()
     {
-
+      $this->assets->addCss("css/success.css");
     }
 
 }
